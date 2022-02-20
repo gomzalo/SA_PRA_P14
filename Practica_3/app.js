682dcfe87ac29bfce7c5479c651aaa90e2f7f11e
A <201318652@ingenieria.usac.edu.gt>
2022-02-19 20:53:01 -0600

    Suma funcionando

--- Practica_3/app.js
+++ Practica_3/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const sum = require('./sum');
+const sum = require('./calc/sum.js');
 
 
 const path = __dirname + '/views/';
