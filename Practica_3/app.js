bad9f9e7eb0fc28e797ebd183817d03e8ead14d8
Ebenja <201504530@ingenieria.usac.edu.gt>
2022-02-19 18:19:25 -0600

    test

--- Practica_3/app.js
+++ Practica_3/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const sum = require('./sum');
+
 
 const path = __dirname + '/views/';
 const port = 8080;
