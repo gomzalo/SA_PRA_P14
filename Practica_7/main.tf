997a013acf98a674d1f3716450da96e3c2879ef7
A <201318652@ingenieria.usac.edu.gt>
2022-03-18 18:32:25 -0600

    added .tf files

---
+++ Practica_7/main.tf
@@ -0,0 +1,62 @@
+locals {
+    # https://www.terraform.io/docs/language/values/locals.html
+    instance_name = format("%s-%s", var.instance_name, substr(md5(module.gce-container.container.image), 0, 8))
+}
+
+####################
+##### CONTAINER SETUP
+
+module "gce-container" {
+    # https://github.com/terraform-google-modules/terraform-google-container-vm
+    source = "terraform-google-modules/container-vm/google"
+    version = "~> 2.0"
+
+    container = {
+        image = var.image
+    }
+
+    restart_policy = "Always"
+}
+
+####################
+##### COMPUTE ENGINE
+
+resource "google_compute_instance" "vm" {
+    name = local.instance_name
+    # gcloud compute machine-types list | grep micro | grep us-central1-a
+    # e2-micro / 2 / 1.00
+    # f1-micro / 1 / 0.60
+    # gcloud compute machine-types list | grep small | grep us-central1-a
+    # e2-small / 2 / 2.00
+    # g1-small / 1 / 1.70
+    machine_type = "f1-micro"
+    # If true, allows Terraform to stop the instance to update its properties.
+    allow_stopping_for_update = true
+
+    boot_disk {
+        initialize_params {
+        image = module.gce-container.source_image
+        }
+    }
+
+    network_interface {
+        network = var.network_name
+
+        access_config {}
+    }
+
+    metadata = {
+        gce-container-declaration = module.gce-container.metadata_value
+    }
+
+    labels = {
+        container-vm = module.gce-container.vm_container_label
+    }
+
+    service_account {
+        email = var.client_email
+        scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+        ]
+    }
+}
